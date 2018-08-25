2c749de540661be656241807b9867608b20c531c
Jane Kim <jane@janekim.me>
2018-08-25 17:57:53 -0400

    simple next app

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default () => (
+  <div>
+    <p>This is the about page</p>
+    <Link href='/'><a>home</a></Link>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <p>Hello Next.js</p>
+    <Link href='/about'><button>about</button></Link>
+  </div>
+)
+
+export default Index
